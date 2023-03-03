4d985143b032cad15e87d1f8e27c33c3e353527f
Benji Wallis <benjiwallis2008@gmail.com>
2023-03-03 22:49:47 +1300

    Redesinged web interface.

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    './*.html',
+    './**/*.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        custom: {
+          'text': '#FFFFFF',
+          'pri': '#114255',
+          'action': '#6792A2',
+          'bg': '#01161E',
+        },
+      },
+    },
+  },
+ variants: {
+  extend: {},
+ },
+ plugins: [],
+ }
